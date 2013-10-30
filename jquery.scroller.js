f79f54cc177796511a4c2832c282160998ce4351
ryanstockau <ryanstockau@users.noreply.github.com>
2013-10-31 00:49:00 +1100

    Added scrolling functions

--- jquery.scroller.js
+++ jquery.scroller.js
@@ -18,6 +18,7 @@ jQuery(document).ready(function($){
 					obj = $(this);
 					e.preventDefault();
 					
+					
 					element_top_offset = obj.attr('data-scroller-offset');
 					if ( element_top_offset === undefined ) {
 						element_top_offset = 0;
@@ -53,5 +54,41 @@ jQuery(document).ready(function($){
 		}
 		
 	});
+	
+	
+	
+	function scroll_to_with_hash( target, offset, duration ) {
+		/*target = target.replace('#', ''); //( target.indexOf("#") + 1); // Remove # 
+		temp_target = target+'_temporaryfix';
+		$('#'+target).attr('id',temp_target);
+		window.location.hash = target;
+		scroll_to( '#'+temp_target );
+		$('#'+temp_target).attr('id',target);*/
+		
+		target = target.replace('#', ''); //( target.indexOf("#") + 1); // Remove # 
+		temp_target = target+'temporaryfixignore123';
+		$('#'+target).attr('id',temp_target);
+		window.location.hash = target;
+		$('#'+temp_target).attr('id',target);
+		scroll_to( '#'+target, offset );
+	}
+	
+	function scroll_to( target, offset, duration ) {
+		var letters = /^[\.#0-9a-zA-Z-]+$/; 
+		if ( !target.match(letters) ) {
+			console.log('Not scrolling to element - invalid target name "' + target + '"');
+			return;
+		}
+		if ( $(target).length ) {
+			scroll_point = $(target).offset().top + offset;
+			// console.log( 'Scrolling to ' + scroll_point + 'px' );
+			$('html, body').animate({
+				scrollTop: (scroll_point)
+			}, duration);		
+		} else {
+			console.log('Not scrolling to element - target "' + target + '" doesn\'t exist');
+		}
+	}
+	
 	
 });
